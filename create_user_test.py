a62330bf47b520cb4635be25795dc3d62613ec8e
Serega-93 <sergejfilipcenko721@gmail.com>
2024-12-10 10:12:51 +0500

    Update create_user_test.py

--- create_user_test.py
+++ create_user_test.py
@@ -3,35 +3,19 @@ import data
 from data import user_body
 from sender_stand_request import response
 
-
-# эта функция меняет значения в параметре firstName
 def get_user_body(first_name):
-    # копирование словаря с телом запроса из файла data, чтобы не потерять данные в исходном словаре
     current_body = data.user_body.copy()
-    # изменение значения в поле firstName
     current_body["firstName"] = first_name
-    # возвращается новый словарь с нужным значением firstName
     return current_body
 
 def positive_assert(first_name):
-    # В переменную user_body сохраняется обновлённое тело запроса
     user_body = get_user_body(first_name)
-    # В переменную user_response сохраняется результат запроса на создание пользователя:
     user_response = sender_stand_request.post_new_user(user_body)
-
-    # Проверяется, что код ответа равен 201
     assert user_response.status_code == 201
-    # Проверяется, что в ответе есть поле authToken и оно не пустое
     assert user_response.json()["authToken"] != ""
-
-    # В переменную users_table_response сохраняется результат запроса на получение данных из таблицы user_model
     users_table_response = sender_stand_request.get_users_table()
-
-    # Строка, которая должна быть в ответе
     str_user = user_body["firstName"] + "," + user_body["phone"] + "," \
                + user_body["address"] + ",,," + user_response.json()["authToken"]
-
-    # Проверка, что такой пользователь есть и он единственный
     assert users_table_response.text.count(str_user) == 1
 
 def negative_assert_symbol(first_name):
@@ -76,23 +60,13 @@ def test_create_user_has_special_symbol_in_first_name_get_error_response():
 def test_create_user_has_number_in_first_name_get_error_response():
     negative_assert_symbol("Aaa1aaa")
 
-# Тест 10. Ошибка
-# В запросе нет параметра firstName
 def test_create_user_no_first_name_get_error_response():
-    # Копируется словарь с телом запроса из файла data в переменную user_body
-    # Иначе можно потерять данные из исходного словаря
     user_body = data.user_body.copy()
-    # Удаление параметра firstName из запроса
     user_body.pop("firstName")
-    # Проверка полученного ответа
     negative_assert_no_first_name(user_body)
 
-# Тест 11. Ошибка
-# Параметр fisrtName состоит из пустой строки
 def test_create_user_empty_first_name_get_error_response():
-    # В переменную user_body сохраняется обновлённое тело запроса
     user_body = get_user_body("")
-    # Проверка полученного ответа
     negative_assert_no_first_name(user_body)
 
 def test_create_user_number_type_first_name_get_error_response():
